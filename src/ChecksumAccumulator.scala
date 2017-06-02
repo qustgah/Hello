8e74095fafacb8880ecf726a4bd4c2d44f0138af
gaoahui <gaoahui@tiqiaamail.com>
2017-06-02 14:55:23 +0800

    Initial commit

---
+++ src/ChecksumAccumulator.scala
@@ -0,0 +1,33 @@
+/**
+  * Created by Administrator on 2017/6/2.
+  */
+class ChecksumAccumulator {
+
+  private var sum = 0
+  def add(b:Byte) :Unit = sum += b
+  def checksum():Int = ~(sum & 0xff) + 1
+
+}
+object ChecksumAccumulator{
+  private var cache = Map[String,Int]()
+  def calculate(s:String) :Int =
+    if (cache.contains(s))
+      cache(s)
+    else{
+      val acc = new ChecksumAccumulator
+      for (c <- s) acc.add(c.toByte)
+      val cs = acc.checksum()
+      cache += (s -> cs)
+      cs
+    }
+
+  def main(args: Array[String]): Unit = {
+    calculate("hello")
+    for(s <- cache){
+      println(s)
+    }
+  }
+
+
+
+}
